c0b5e33d4765083c6b358ae66bc0d3312571c8b6
Islom <radjapov_islom@mail.ru>
2022-12-17 20:45:15 +0500

    work

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,6 @@
 #Create a varaible 'number' and assign it the value of 43.
-
+number = range(1, 44)
 #Create a variable 'answer' and assign it the sum of the number's digits.
-
+answer = sum(number)
 #Print the vallue of the 'answer'.
+print(answer)
